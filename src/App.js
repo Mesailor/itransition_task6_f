6c7729d9b8659877f7835fea14b0fa1eacdc4810
MeSailor <themicroline@gmail.com>
2024-02-23 17:18:31 +0400

    user can drow

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ overflow: "hidden" }}>
       <RouterProvider router={router} />
     </div>
   );
--- src/pages/BoardPage.js
+++ src/pages/BoardPage.js
@@ -1,12 +1,42 @@
+import { useEffect, useState } from "react";
 import Toolbar from "../components/Toolbar";
 
 export default function BoardPage() {
+  const [c, setC] = useState({});
+  const [ctx, setCtx] = useState({});
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  function startDraw(e) {
+    setIsDrawing(true);
+    ctx.moveTo(e.clientX, e.clientY - e.target.offsetTop);
+  }
+
+  function finishDraw() {
+    setIsDrawing(false);
+  }
+
+  function draw(e) {
+    if (!isDrawing) return;
+    ctx.lineTo(e.clientX, e.clientY - e.target.offsetTop);
+    ctx.stroke();
+    ctx.moveTo(e.clientX, e.clientY - e.target.offsetTop);
+  }
+
+  useEffect(() => {
+    const c = document.getElementById("my-canvas");
+    c.width = window.innerWidth;
+    c.height = window.innerHeight - c.offsetTop;
+    setC(c);
+    setCtx(c.getContext("2d"));
+  }, []);
+
   return (
     <div className="board">
       <Toolbar />
       <canvas
-        width={600}
-        height={400}
+        onMouseMove={draw}
+        onMouseDown={startDraw}
+        onMouseUp={finishDraw}
         id="my-canvas"
         style={{
           height: "calc(100vh - 4.5rem)",
